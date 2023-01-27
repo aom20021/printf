abac36549d1615e49de768148ab57d2b1b676d97
Andres Ollero Morales <anollero@c2r19s5.42madrid.com>
2023-01-27 10:57:13 +0100

    %p

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: anollero <anollero@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 16:00:46 by anollero          #+#    #+#             */
-/*   Updated: 2023/01/26 13:49:47 by anollero         ###   ########.fr       */
+/*   Updated: 2023/01/27 10:54:01 by anollero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,7 @@
 #include "include/libft.h"
 #include "stdio.h"
 
-int size_unsigned_num_hex(unsigned int num)
-{
-    int i = 0;
-    while (num != 0)
-    {
-        num = num / 16;
-        i++;
-        if (num % 10 == 0 && num < 100 && num != 0)
-        {
-            i++;
-        }
-    }
-    return i;
-}
-
-char *atoh(unsigned int num)
-{
-    char *res = calloc(size_unsigned_num_hex(num), 1);
-    int i = size_unsigned_num_hex(num);
-    char hex[16] = "0123456789ABCDEF";
-    while (i != 0)
-    {
-        res[i - 1] = hex[num % 16];
-        num = num / 16;
-        i--;
-    }
-    return res;
-}
-
-
-char	*ft_utoa_aux()
+char	*ft_utoa_aux(void)
 {
 	char	*res;
 
@@ -56,17 +26,10 @@ char	*ft_utoa_aux()
 	return (res);
 }
 
-void	ft_sign_ualloc(unsigned int *aux, unsigned int *n, char *sign, int *alloc)
+void	ft_sign_ualloc(unsigned int *aux, char *sign, int *alloc)
 {
 	*(alloc) = 1;
-	if (*n < 0)
-	{
-		*(sign) = '-';
-		*n = -(*(n));
-		*(alloc) = *(alloc) + 1;
-	}
-	else
-		*(sign) = '\0';
+	*(sign) = '\0';
 	while (*aux != 0)
 	{
 		*aux = *(aux) / 10;
@@ -84,7 +47,7 @@ char	*ft_utoa(unsigned int n)
 	aux = n;
 	if (n == 0)
 		return (ft_utoa_aux());
-	ft_sign_ualloc(&aux, &n, &sign, &alloc);
+	ft_sign_ualloc(&aux, &sign, &alloc);
 	res = (char *)malloc(alloc * sizeof(char));
 	if (res == NULL)
 		return (NULL);
@@ -102,10 +65,54 @@ char	*ft_utoa(unsigned int n)
 	return (res);
 }
 
+int	ft_sizehex(unsigned long num)
+{
+	int size;
+
+	size = 0;
+	while (num != 0)
+	{
+		num = num / 16;
+		size++;
+		if ((num != 0) && (num % 10 == 0) && (num < 100))
+		{
+			size = size + 2;
+			num = 0;
+		}
+	}
+	return (size);
+}
+
+char	*ft_atoh(unsigned long num, int mode)
+{
+	char	*str;
+	char	*hex;
+	int		i;
+
+	i = ft_sizehex(num) - 1;
+	hex = "0123456789abcdef";
+	if (num == 0)
+	{
+		str = ft_calloc(2, 1);
+		str[0] = '0';
+		return (str);
+	}
+	str = ft_calloc(ft_sizehex(num) + 1, 1);
+	while (num != 0)
+	{
+		str[i] = hex[num % 16];
+		if (mode == 1)
+			str[i] = ft_toupper(str[i]);
+		i--;
+		num = num / 16;
+	}
+	return (str);
+}
+
 void	ft_formatsel(char format, int *length, va_list pa)
 {
-	char carac;
-	char *string;
+	char	carac;
+	char	*string;
 
 	if (format == 'c')
 	{
@@ -142,17 +149,31 @@ void	ft_formatsel(char format, int *length, va_list pa)
 	}
 	else if (format == 'x' || format == 'X')
 	{
-		ft_putstr_fd(atoh(va_arg(pa, int)), 1);
+		char *str;
+		if (format == 'x')
+			str = ft_atoh(0xabcdef, 0);
+		else
+			str = ft_atoh(0xabcdef, 1);
+		ft_putstr_fd(str, 1);
+		(*length) = *length + ft_strlen(str);
+		free(str);
 	}
 	else if (format == 'u')
 	{
-		unsigned int aux = va_arg(pa, unsigned int);
-		if (aux < 0)
-			aux = 4294967286 - aux;
+		unsigned int aux = va_arg(pa, int);
 		char *ret2 = ft_utoa(aux);
 		(*length) = *length + ft_strlen(ret2);
+		ft_putstr_fd(ret2, 1);
 		free(ret2);
-		ft_putnbr_fd(aux, 1);
+	}
+	else if (format == 'p')
+	{
+		char *str;
+		ft_putstr_fd("0x", 1);
+		str = ft_atoh(va_arg(pa, unsigned long), 0);
+		ft_putstr_fd(str, 1);
+		(*length) = *length + ft_strlen(str) + 2;
+		free(str);
 	}
 }
 
